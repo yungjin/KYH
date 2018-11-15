bb3d6015046e3bb06ca622c0ada54e5fb194bde3
영진 최 <chyj0318@naver.com>
2018-11-15 10:11:34 +0900

    Y 최신

--- KYH/FormY.cs
+++ KYH/FormY.cs
@@ -148,4 +148,100 @@ namespace KYH
             return true;
         }
     }
+    public class Items
+    {
+        string col1;
+        string col2;
+        string col3;
+        string col4;
+        string col5;
+        string col6;
+        string col7;
+        public Items(string[] a)
+        {
+            col1 = a[0];
+            col2 = a[1];
+            col3 = a[2];
+            col4 = a[3];
+            col5 = a[4];
+            col6 = a[5];
+            col7 = a[6];
+        }
+
+        public string getCol1()
+        {
+            return col1;
+        }
+        public string getCol2()
+        {
+            return col2;
+        }
+        public string getCol3()
+        {
+            return col3;
+        }
+        public string getCol4()
+        {
+            return col4;
+        }
+        public string getCol5()
+        {
+            return col5;
+        }
+        public string getCol6()
+        {
+            return col6;
+        }
+        public string getCol7()
+        {
+            return col7;
+        }
+    } //아이템
+
+    public class Items2
+    {
+        string col1;
+        string col2;
+        string col3;
+        string col4;
+        string col5;
+        string col6;
+        
+        public Items2(string[] a)
+        {
+            col1 = a[0];
+            col2 = a[1];
+            col3 = a[2];
+            col4 = a[3];
+            col5 = a[4];
+            col6 = a[5];
+            
+        }
+
+        public string getCol1()
+        {
+            return col1;
+        }
+        public string getCol2()
+        {
+            return col2;
+        }
+        public string getCol3()
+        {
+            return col3;
+        }
+        public string getCol4()
+        {
+            return col4;
+        }
+        public string getCol5()
+        {
+            return col5;
+        }
+        public string getCol6()
+        {
+            return col6;
+        }
+        
+    } //아이템
 }
